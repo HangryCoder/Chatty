d7e29e8c3fa1c9fbd91df22bed17ad5e8a4dc76a
Sonia <soniawadji@gmail.com>
2023-02-22 14:54:17 +0530

    Created Home Screen. Added firebase

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
-        <LoginScreen />
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
