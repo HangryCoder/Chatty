a4a21036aeca20de44e16cc2470c01a4328ff236
Sonia <soniawadji@gmail.com>
2023-02-23 12:55:17 +0530

    Search is working somewhat

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,55 +2,73 @@ import React from 'react';
 import { StyleSheet, View, Text, FlatList, TouchableOpacity, TextInput } from 'react-native';
 import GroupItem from '../components/GroupItem'
 
+const dummyData = [
+    {
+        id: 1,
+        title: 'Group 1',
+        memberCount: 10,
+        icon: '',
+        joined: false
+    },
+    {
+        id: 2,
+        title: 'Group 2',
+        memberCount: 20,
+        icon: '',
+        joined: true
+    },
+    {
+        id: 3,
+        title: 'Group 3',
+        memberCount: 30,
+        icon: '',
+        joined: true
+    },
+    {
+        id: 4,
+        title: 'Group 4',
+        memberCount: 40,
+        icon: '',
+        joined: false
+    },
+    {
+        id: 5,
+        title: 'Group 5',
+        memberCount: 50,
+        icon: '',
+        joined: true
+    },
+]
+
 class HomeScreen extends React.Component {
 
     constructor() {
         super()
         this.state = {
             searchValue: "",
-            data: [
-                {
-                    id: 1,
-                    title: 'Group 1',
-                    memberCount: 10,
-                    icon: '',
-                    joined: false
-                },
-                {
-                    id: 2,
-                    title: 'Group 2',
-                    memberCount: 20,
-                    icon: '',
-                    joined: true
-                },
-                {
-                    id: 3,
-                    title: 'Group 3',
-                    memberCount: 30,
-                    icon: '',
-                    joined: true
-                },
-                {
-                    id: 4,
-                    title: 'Group 4',
-                    memberCount: 40,
-                    icon: '',
-                    joined: false
-                },
-                {
-                    id: 5,
-                    title: 'Group 5',
-                    memberCount: 50,
-                    icon: '',
-                    joined: true
-                },
-            ]
+            filteredData: dummyData
         }
+        this.originalData = dummyData
     }
 
-    onChangeText = (text) => {
+    /* onChangeText = (text) => {
+         console.log("text " + text)
+         this.setState({ searchValue: text })
+     }*/
+
+    searchFunction = (text) => {
         console.log("text " + text)
-    }
+        if (text.trim() == "") {
+            this.setState({ data: dummyData, searchValue: text });
+            return
+        }
+        const updatedData = this.originalData.filter((item) => {
+            const itemData = `${item.title.toUpperCase()})`;
+            const searchData = text.toUpperCase();
+            return itemData.startsWith(searchData)//indexOf(searchData) > -1;
+        });
+        this.setState({ filteredData: updatedData, searchValue: text });
+    };
 
     renderSearchBar = () => {
         return (<View style={{
@@ -67,7 +85,7 @@ class HomeScreen extends React.Component {
                 editable
                 numberOfLines={1}
                 placeholder="Search"
-                onChangeText={text => onChangeText(text)}
+                onChangeText={text => this.searchFunction(text)}
                 value={this.state.searchValue}
                 style={{ flex: 1, marginHorizontal: 8, fontSize: 14, color: '#A1A1A1' }}
             />
@@ -75,7 +93,7 @@ class HomeScreen extends React.Component {
     }
 
     createGroup = () => {
-        let newData = [...this.state.data]
+        let newData = [...this.state.filteredData]
         newData.push({
             id: 6,
             title: 'Group 6',
@@ -84,7 +102,7 @@ class HomeScreen extends React.Component {
             joined: true
         })
         this.setState({
-            data: newData
+            filteredData: newData
         })
     }
 
@@ -101,7 +119,7 @@ class HomeScreen extends React.Component {
     renderGroupList = () => {
         return <FlatList
             ItemSeparatorComponent={this.renderItemSeparator()}
-            data={this.state.data}
+            data={this.state.filteredData}
             renderItem={({ item }) => <GroupItem group={item} />}
             keyExtractor={item => item.id}
         />
