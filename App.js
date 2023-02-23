fb939645f463c972eeb3702298b4a439e4454249
Sonia <soniawadji@gmail.com>
2023-02-23 14:47:35 +0530

    Removed Login header

--- App.js
+++ App.js
@@ -11,8 +11,8 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Login" component={LoginScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
