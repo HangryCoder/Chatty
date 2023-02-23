3a75e2c995f33e185811b83794c25f27d68b5dec
Sonia <soniawadji@gmail.com>
2023-02-23 14:51:42 +0530

    Removed Group header

--- App.js
+++ App.js
@@ -11,8 +11,9 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="Home" component={HomeScreen} />
+
             </Stack.Navigator>
         </NavigationContainer>
     );
